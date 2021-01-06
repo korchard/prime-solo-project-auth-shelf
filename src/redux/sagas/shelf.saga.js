a7496ffc974df4fd455d4364d80394716f1572a1
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-01-06 15:49:53 -0600

    completed the delete generator function

--- src/redux/sagas/shelf.saga.js
+++ src/redux/sagas/shelf.saga.js
@@ -31,10 +31,26 @@ function* fetchShelf() {
       console.log('Shelf post request failed', error);
     }
   }
+
+  // DELETE ROUTE
+  function* deleteItem (action) {
+    try {
+      const config = {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+      };
+  
+      yield axios.post(`api/shelf/${action.payload.id}`, action.payload, config);
+      yield put({ type: 'FETCH_SHELF' });
+    } catch (error) {
+      console.log('Shelf delete request failed', error);
+    }
+  }
   
   function* shelfSaga() {
     yield takeLatest('FETCH_SHELF', fetchShelf);
     yield takeLatest('ADD_ITEM', addItem);
+    yield takeLatest('DELETE_ITEM', deleteItem);
   }
   
   export default shelfSaga;
